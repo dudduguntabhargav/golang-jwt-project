606be7ea9d4c48aa4213029136abd872551e1ccb
bhargavduddugunta <duddugunta.bhargavreddy@gmail.com>
2024-12-24 16:14:51 -0500

    "server setup"

--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 import(
 	"github.com/gin-gonic/gin"
+	routes "github.com/bhargavduddugunta/golang-jwt-project/routes"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
 )
+
+func main(){
+
+			err := godotenv.Load(".env")
+			if err!=nil{
+				log.Fatal("error in loading .env file")
+			}
+
+			port := os.Getenv("PORT")
+			if port==""{
+				port = "8000"
+			}
+			
+			router := gin.New()
+			router.Use(gin.Logger())
+
+			routes.UserRoutes(router)
+      routes.AuthRoutes(router)
+
+			router.GET("api-1/", func(c *gin.Context){
+				c.JSON(200, gin.H{"success": "Access granted for api-1"})
+			})
+
+			router.GET("api-2/", func(c *gin.Context){
+				c.JSON(200, gin.H{"success": "Access granted for api-1"})
+			})
+
+			router.Run(":" + port)
+}
