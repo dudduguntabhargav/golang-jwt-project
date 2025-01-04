b03bb04ba63b015238c4043dd4f8d5814b7952fe
bhargavduddugunta <duddugunta.bhargavreddy@gmail.com>
2025-01-04 08:30:15 -0500

    controller changes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/bhargavduddugunta/golang-jwt-project/controllers"
+	"github.com/bhargavduddugunta/golang-jwt-project/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.UserAuthenticate())
+	incomingRoutes.POST("/userdata",controller.GetUsers())
+	incomingRoutes.POST("/users/:user_id",controller.GetUser())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	controller "github.com/bhargavduddugunta/golang-jwt-project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserAuthenticate(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("users/signup",controller.Signup())
+	incomingRoutes.POST("users/login",controller.Login())
+}
